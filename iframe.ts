7f29ab01e544b039f41ed5820428936d3aebf41c
Anton <anton@MacBook-Pro-Anton.local>
2017-12-06 12:05:23 +0300

    sada

---
+++ iframe.ts
@@ -0,0 +1,34 @@
+
+import { IframeMessager } from './messager';
+const { iframeResizer } = require('iframe-resizer');
+
+
+{
+	const url = 'https://strahovki24.dwpr.ru/iframe-integration/travel/insurance';
+	const strahovki24Iframe = document.createElement('iframe');
+	const iframeMessager = new IframeMessager('strahovki-24-iframe');
+	const container = document.getElementById('strahovki-24-iframe');
+	strahovki24Iframe.src = url;
+	strahovki24Iframe.scrolling = 'no';
+	strahovki24Iframe.name = 'strahovki-24-iframe';
+
+	container.appendChild(strahovki24Iframe);
+
+	iframeMessager.on('STRAHOVKI_24_LOADED', (payload) => {
+		iframeResizer({
+			sizeHeight: true,
+			sizeWidth: true,
+			warningTimeout: 15000
+		}, strahovki24Iframe);
+		iframeMessager.send('RESIZE', { width: innerWidth });
+		iframeMessager.send('INIT', {
+			url: window.location.href
+		});
+		window.addEventListener('resize', (event) => iframeMessager.send('RESIZE', { width: innerWidth }) );
+	});
+	iframeMessager.on('STRAHOVKI_REDIRECT', (payload) => { 
+		console.log(payload.url);
+		// window.location.replace(payload.url)
+	});
+
+}
---
+++ messager.ts
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+export class IframeMessager {
+
+	/**
+	 * Creates an instance of IframeMessager.
+	 * @param {string} [name='strahovki-24-iframe'] 
+	 * @memberof IframeMessager
+	 */
+	constructor(public name = 'strahovki-24-iframe') {
+		window.addEventListener('message', this.listen);
+	}
+
+	/**
+	 * 
+	 * @memberof IframeMessager
+	 */
+	public listeners = [];
+
+	/**	
+	 * 
+	 * @readonly
+	 * @memberof IframeMessager
+	 */
+	public get frame() : Window {
+		if (this.name)  return window.frames[this.name];
+		return window.frames;
+	}
+
+	
+	/**
+	 * 
+	 * @param {string} type 
+	 * @param {*} [payload={}] 
+	 * @memberof IframeMessager
+	 */
+	public send(type : string, payload : any = {}) {
+		this.frame.postMessage({ type, payload }, '*');
+	}
+
+
+	/**
+	 * 
+	 * @memberof IframeMessager
+	 */
+	private listen = (event) => {
+		if (typeof event.data == 'object') {
+			const listener = this.listeners.find(listener => listener.type == event.data.type);
+			if (listener) listener.handle(event.data);
+		}
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @memberof IframeMessager
+	 */
+	public on = (type : string = '', handle = (payload) => {}) => {
+		this.listeners.push({
+			type,
+			handle
+		});
+	}
+
+}
